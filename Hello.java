be4d108d2044de1cc48ebb0120a3453995d33b24
NANDU1156 <nandinibadenandu@gmail.com>
2026-09-07 14:10:41 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,54 @@
+/*package Demo;
+public class Hello {
+	public static void main(String[] args) {
+		int [] arr = {10, 20, 30, 40, 50};
+		int value = arr[2];
+		System.out.println("Value = " + value);
+	}
+
+}*/
+/*package Demo;
+public class Hello {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				System.out.println(i + " " + j);
+			}
+		}
+	}
+
+}*/
+/*package Demo;
+public class Hello {
+	public static void main(String[] args) {
+		int[] arr = {2,8,3,7,5,5};
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] + arr[j] == 10) {
+				System.out.println(arr[i] + " + " + arr[j]  + " =10");
+			    }
+			}
+		}
+	}
+}
+*/
+/*package Demo;
+public class Hello {
+	public static void main(String[] args) {
+		int[] arr = {10, 20, 30, 40, 50};
+		boolean sorted = true;
+		for (int i = 0; i < arr.length -1;i++) {
+			if (arr[i] > arr[i + 1]) {
+				sorted  = false;
+				break;
+			}
+		}
+		if (sorted) {
+			System.out.println("Sorted");
+		}else {
+			System.out.println("Not Sorted");
+		}
+	}
+}
+*/
